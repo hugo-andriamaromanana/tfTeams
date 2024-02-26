b179120e55678eb5f8d26dadd5f04197c0c919df
yayassad <alexandre.assad@laplateforme.io>
2024-02-26 19:13:45 +0100

    :fix: centralize parsers function & migrate parser in package

--- tfTeams/parser/dataset_parsers.py
+++ tfTeams/parser/dataset_parsers.py
@@ -5,3 +5,8 @@ def parse_csv_champions(champ_dataset: Path) -> DataFrame:
     dataframe = read_csv(champ_dataset)
     dataframe["synergies"] = dataframe["synergies"].str.split()
     return dataframe
+
+def parse_csv_synergies(syn_dataset: Path) -> DataFrame: 
+    dataframe = read_csv(syn_dataset)
+    dataframe["stades"] = dataframe["stades"].str.split()
+    return dataframe
